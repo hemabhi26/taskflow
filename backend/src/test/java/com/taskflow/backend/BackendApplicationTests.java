9ec47a040d3949ffd3973d0c2675313615fa65d1
hemabhi26 <hemanthabhinav2601@gmail.com>
2026-03-27 19:45:39 +0530

    fix: update testing, flyway

--- backend/src/test/java/com/taskflow/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/taskflow/backend/BackendApplicationTests.java
@@ -1,19 +1,7 @@
 package com.taskflow.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:mysql://localhost:3306/taskflowdb?createDatabaseIfNotExist=true&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
-    "spring.datasource.username=root",
-    "spring.datasource.password=taskflow123",
-    "spring.jpa.hibernate.ddl-auto=none",
-    "spring.flyway.enabled=true",
-    "spring.flyway.baseline-on-migrate=true",
-    "spring.flyway.out-of-order=true"
-})
 class BackendApplicationTests {
 
     @Test
